b2b46672546ca716c43892fe3559de48e5d86128
mabreu0 <moquete.abreu@gmail.com>
2022-06-16 03:07:11 -0400

    about menu link module for quicker site bootstraping

---
+++ drupal8/modules/about_menu/src/Controller/AboutUsOptionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\about_menu\Controller;
+
+/**
+* Generic about us option for main menu - simple web page!
+*
+*/
+
+class AboutUsOptionController {
+
+	public function aboutUsOption() {
+		return [
+			"#markup" => "About us! ....."
+		];
+	}
+}
+
+?>
